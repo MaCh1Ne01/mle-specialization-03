a402796efc39f3447dfc4e3c8c358166a5c5bc41
Marcos <marcos@MLES.UbuntuPC>
2025-11-13 02:34:05 -0500

    Changes are uploaded.

--- notebooks/constants.py
+++ notebooks/constants.py
@@ -1,34 +1,35 @@
-TARGET = None
+TARGET = "Close"
 CUTOFF_DATE = "2023-09-15"
-TEST_PERCENTAGE = 0
+TEST_PERCENTAGE = 0.2
+SAMPLE_PERCENTAGE = 0.01
 SEED = 2
-DROP_FEATURES = ["ID","Z_CostContact","Z_Revenue"]
-SCALING_METHOD = "Robust"
+DROP_FEATURES = []
+SCALING_METHOD = "Standard"
 REPOSITORY_OWNER = "MaCh1Ne01"
-REPOSITORY_NAME = "mle-specialization-02"
-MLFLOW_DAGSHUB_URL = "https://dagshub.com/MaCh1Ne01/mle-specialization-02.mlflow"
-EXPERIMENT_NAME = "Customer Segmentation - MLE 02 Project"
+REPOSITORY_NAME = "mle-specialization-03"
+MLFLOW_DAGSHUB_URL = "https://dagshub.com/MaCh1Ne01/mle-specialization-03.mlflow"
+EXPERIMENT_NAME = "Companies Stock Forecasting - MLE 03 Project"
 OBJECT_TO_NUMERICAL_FEATURES = ["Year_Birth","Teenhome"]
-INITIAL_OBJECT_FEATURES = ["Year_Birth","Education","Marital_Status","Teenhome","Dt_Customer"]
-INITIAL_NUMERICAL_FEATURES = ["Year_Birth","Income","Kidhome","Teenhome","Recency","MntWines","MntFruits","MntMeatProducts",\
-                      "MntFishProducts","MntSweetProducts","MntGoldProds","NumDealsPurchases","NumWebPurchases","NumCatalogPurchases",\
-                      "NumStorePurchases","NumWebVisitsMonth","AcceptedCmp2","Response"]
-NOMINAL_FEATURES = ["Marital_Status"]
+INITIAL_OBJECT_FEATURES = ["Date","Company"]
+INITIAL_NUMERICAL_FEATURES = ["Open","High","Low","Close","Volume","Dividends","Stock_Splits"]
+NOMINAL_FEATURES = ["Company"]
 ORDINAL_FEATURES = ["Education"]
 NUMERICAL_FEATURES = ["Customer_Age","Income","Kidhome","Teenhome","Customer_Tenure","Recency","MntWines","MntFruits","MntMeatProducts",\
                       "MntFishProducts","MntSweetProducts","MntGoldProds","NumDealsPurchases","NumWebPurchases","NumCatalogPurchases",\
                       "NumStorePurchases","NumWebVisitsMonth","AcceptedCmp2","Response"]
 DATE_FEATURES_TRANSFORM = [("Year_Birth","years"),("Dt_Customer","days")]
 DATE_FEATURES_RENAMED = {"Year_Birth": "Customer_Age", "Dt_Customer": "Customer_Tenure"}
-ID_FEATURE = "customer_marketing_campaign_id"
+DATE_FEATURES = ["Date"]
+ID_FEATURE = "companies_stock_id"
 EXCLUDED_FEATURES = [ID_FEATURE] + ["event_timestamp","created"]
-STRATIFY_FEATURE = None
+STRATIFY_FEATURE = "Company"
 CUSTOM_ORDER = {
     "Education": ["Basic","2n Cycle","Graduation","Master","PhD"]
 }
-FEAST_REPOSITORY_PATH = "../feast_service/fs_mle_02/feature_repo/"
-BASE_MODEL_NAME = "Gaussian Mixture"
-MODEL_01_NAME = "K-Means"
-MODEL_02_NAME = "OPTICS+K-Means Ensemble"
+FEAST_REPOSITORY_PATH = "/feast_service/fs_mle_03/feature_repo/"
+BASE_MODEL_NAME = "Linear Regression"
+MODEL_01_NAME = "LightGBM Regressor"
+MODEL_02_NAME = "XGBoost Regressor"
+MODEL_03_NAME = "Ridge Stacking Ensemble"
 TRAINING_DATA_LABEL = "Training Data"
-TESTING_DATA_LABEL = None
+TESTING_DATA_LABEL = "Testing Data"
